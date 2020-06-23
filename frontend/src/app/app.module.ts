d9c1c250bcf2ad13e3469734de9f9106169267eb
Pazeto22 <gustavopazeto95@gmail.com>
2020-06-23 16:45:55 -0300

    Feat: Navbar, Footer and Background

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { MaterialModule } from "./material/material.module";
+import { NavbarComponent } from './ui/navbar/navbar.component';
+import { FooterComponent } from './ui/footer/footer.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
