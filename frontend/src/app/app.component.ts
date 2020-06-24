c8f165341319150ed7f71167142756fa51036427
Pazeto22 <gustavopazeto95@gmail.com>
2020-06-24 04:10:53 -0300

    Feat: News on card

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,7 +7,10 @@ import { Component } from "@angular/core";
     <div id="container">
       <div class="bannerLogo"><img src="assets/img/bannerLogo.png" /></div>
       <router-outlet>
-        <app-post-card></app-post-card>
+        <mat-card>
+          <app-post-card></app-post-card>
+          <app-post-news></app-post-news>
+        </mat-card>
       </router-outlet>
     </div>
     <div id="content-wrap">
