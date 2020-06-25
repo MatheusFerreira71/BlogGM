e5ee534a4170b3c00ec95803e9360a63ce57b3e3
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2020-06-25 09:27:24 -0300

    Refactory de tags com Promise

--- server/src/controllers/TagController.js
+++ server/src/controllers/TagController.js
@@ -1,24 +1,30 @@
 const Tag = require('../models/Tag');
 const PostTag = require('../models/PostTag');
+const countPosts = async (tags) => {
+    const tages = tags.map(tag => {
+        const { _id, titulo } = tag;
+        return { _id, titulo, qtdePosts: 0 };
+    })
+    const postTages = await PostTag.find();
 
+    for (const pt of postTages) {
+        for (const tag of tages) {
+            if (String(tag._id) == String(pt.tagId)) {
+                tag.qtdePosts++
+            }
+        }
+    }
+    return new Promise(resolve => {
+        resolve(tages);
+    })
+}
 module.exports = {
     index: async (req, res) => {
         try {
             const tags = await Tag.find().sort({ titulo: 'asc' });
-            const tages = tags.map(tag => {
-                const { _id, titulo } = tag;
-                return { _id, titulo, qtdePosts: 0 };
-            })
-            const postTages = await PostTag.find();
-
-            for (const pt of postTages) {
-                for (const tag of tages) {
-                    if (String(tag._id) == String(pt.tagId)) {
-                        tag.qtdePosts++
-                    }
-                }
-            }
-            res.json(tages);
+            countPosts(tags).then(response => {
+                res.json(response)
+            });
         } catch (erro) {
             console.log(erro);
             res.status(500).send(erro);
@@ -41,20 +47,9 @@ module.exports = {
             const tags = await Tag.find({
                 titulo: { $regex: ".*" + titulo + ".*" }
             }).sort({ titulo: 'asc' });
-            const tages = tags.map(tag => {
-                const { _id, titulo } = tag;
-                return { _id, titulo, qtdePosts: 0 };
-            })
-            const postTages = await PostTag.find();
-
-            for (const pt of postTages) {
-                for (const tag of tages) {
-                    if (String(tag._id) == String(pt.tagId)) {
-                        tag.qtdePosts++
-                    }
-                }
-            }
-            res.json(tages);
+            countPosts(tags).then(response => {
+                res.json(response)
+            });
         } catch (erro) {
             console.log(erro);
         }
