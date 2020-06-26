7f771784fbf78aeb4d620c460550e82a168a79c8
Pazeto22 <gustavopazeto95@gmail.com>
2020-06-26 05:16:51 -0300

    Refactor: Recriação de navbar e footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
+  // template: `
+  //   <app-navbar></app-navbar>
+  //   <router-outlet>
+  //     <app-home></app-home>
+  //   </router-outlet>
+  //   <div id="content-wrap">
+  //     <app-footer></app-footer>
+  //   </div>
+  // `,
   template: `
     <app-navbar></app-navbar>
-    <router-outlet>
-      <app-home></app-home>
-    </router-outlet>
-    <div id="content-wrap">
-      <app-footer></app-footer>
-    </div>
+    <app-footer></app-footer>
   `,
   styles: [],
 })
