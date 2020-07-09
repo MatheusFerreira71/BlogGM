cb476e31116337199b9181072fe66ad0cdb635e7
Pazeto22 <gustavopazeto95@gmail.com>
2020-07-08 21:59:49 -0300

    Feat: incremento de postcreate

--- frontend/src/app/postcreate/create-form/create-form.component.ts
+++ frontend/src/app/postcreate/create-form/create-form.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit } from "@angular/core";
 import { AngularEditorConfig } from "@kolkov/angular-editor";
+import { COMMA, ENTER } from "@angular/cdk/keycodes";
+import { MatChipInputEvent } from "@angular/material/chips";
+
+export interface TagsCadastro {
+  titulo: string;
+  tituloLower: string;
+}
 @Component({
   selector: "app-create-form",
   templateUrl: "./create-form.component.html",
   styleUrls: ["./create-form.component.scss"],
 })
 export class CreateFormComponent implements OnInit {
-  constructor() {
-    function categoria2check(categoria) {
-      if (categoria.value == "noticia") {
-        document.getElementById("catSecSelector").style.display = "block";
-      } else {
-        document.getElementById("catSecSelector").style.display = "none";
-      }
-    }
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 
@@ -43,4 +42,37 @@ export class CreateFormComponent implements OnInit {
       },
     ],
   };
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  tags: TagsCadastro[] = [{ titulo: "Zelda", tituloLower: "zelda" }];
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || "").trim()) {
+      this.tags.push({
+        titulo: value.trim(),
+        tituloLower: value.trim().toLowerCase(),
+      });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = "";
+    }
+  }
+
+  remove(tag: TagsCadastro): void {
+    const index = this.tags.indexOf(tag);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
 }
