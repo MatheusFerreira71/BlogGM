28d3ef9e738991ca1c82b69d63e7d37069a903b9
Pazeto22 <gustavopazeto95@gmail.com>
2020-07-09 14:34:26 -0300

    Feat: Comentários 100% funcionais

--- frontend/src/app/postagem/comentario/comentario-form/comentario-form.component.ts
+++ frontend/src/app/postagem/comentario/comentario-form/comentario-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { ComentarioCreate, ComentarioService } from "../comentario.service";
 
@@ -9,13 +9,14 @@ import { ComentarioCreate, ComentarioService } from "../comentario.service";
 })
 export class ComentarioFormComponent implements OnInit {
   @Input() postId: string;
+  @Output() criarComentario = new EventEmitter();
   constructor(private comentarioSrv: ComentarioService) {}
 
   ngOnInit(): void {}
   nome: string = "";
   email: string = "";
   texto: string = "";
-  usuario: string = "";
+  usuario: string | undefined = undefined;
 
   create(form: NgForm): void {
     const comentario: ComentarioCreate = {
@@ -25,9 +26,13 @@ export class ComentarioFormComponent implements OnInit {
       texto: this.texto,
       postId: this.postId,
     };
-    console.log(comentario);
     if (form.valid) {
-      this.comentarioSrv.create(comentario).subscribe();
+      this.comentarioSrv.create(comentario).subscribe((coment) => {
+        this.nome = "";
+        this.email = "";
+        this.texto = "";
+        this.criarComentario.emit();
+      });
     } else {
       alert("form Invalido");
     }
--- frontend/src/app/postagem/comentario/comentario.service.ts
+++ frontend/src/app/postagem/comentario/comentario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment as env } from "../../../environments/environment";
+import { Comentario } from "../../postagem/postagem.service";
 
 export interface ComentarioCreate {
   usuario: string | undefined;
